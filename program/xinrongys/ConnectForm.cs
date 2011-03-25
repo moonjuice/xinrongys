1c30329e4e3a135d2a5c785f9e0e7511d829e0ce
moon80362@gmail.com <moon80362@gmail.com@0febd70d-a258-cbe2-6812-9670ac12e6ed>
2011-03-25 05:53:58 +0000

    新增Connector類別 新增連線視窗

---
+++ program/xinrongys/ConnectForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace xinrongys
+{
+    public partial class ConnectForm : Form
+    {
+        private Connector connect;
+        public ConnectForm()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            this.connect = new Connector(this.hostTextBox.Text,this.nameTextBox.Text,this.passwordTextBox.Text);
+            if (!connect.connect())
+                connect = null;
+            else
+                this.Close();
+        }
+
+        public Connector getConnector()
+        {
+            return this.connect;
+        }
+    }
+}
--- program/xinrongys/Connector.cs
+++ program/xinrongys/Connector.cs
@@ -2,10 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
 
 namespace xinrongys
 {
-    class Connector
+    public class Connector
     {
+        #region Member Variables
+        private string host;
+        private string name;
+        private string password;
+        private string dbname = "xinrongys";
+        private MySqlConnection conn;
+        #endregion
+
+        #region Constructors
+        public Connector(string host, string name, string password)
+        {
+            this.host = host;
+            this.name = name;
+            this.password = password;
+        }
+        #endregion
+
+        #region Public Method
+        public bool connect()
+        {
+            bool result = true;
+            if (host == null || name == null || password == null)
+            {
+                MessageBox.Show("無法連線到資料庫");
+                result = false;
+            }
+            else
+            {
+                // 如果有特殊的編碼在database後面請加上;CharSet=編碼, utf8請使用utf8_general_ci 
+                string connStr = "server=" + host + ";uid=" + name + ";pwd=" + password + ";database=" + dbname + ";CharSet=utf8";
+                conn = new MySqlConnection(connStr);
+
+                // 連線到資料庫
+                try
+                {
+                    conn.Open();
+                }
+                catch (MySql.Data.MySqlClient.MySqlException ex)
+                {
+                    switch (ex.Number)
+                    {
+                        case 0:
+                            MessageBox.Show("無法連線到資料庫");
+                            break;
+                        case 1045:
+                            MessageBox.Show("使用者帳號或密碼錯誤,請再試一次");
+                            break;
+                        default:
+                            MessageBox.Show("無法連線到資料庫，錯誤代碼："+ex.Number);
+                            break;
+                    }
+                    result = false;
+                }
+            }
+            return result;
+        }
+        #endregion
+
     }
 }
