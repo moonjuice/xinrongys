cee0e117d765e24129f77f9eb15f0675a0c6e09f
moon80362@gmail.com <moon80362@gmail.com@0febd70d-a258-cbe2-6812-9670ac12e6ed>
2011-04-04 02:34:20 +0000

    Customer類別 增加fax ext3屬性，以及get and set 方法 Customer類別 建構子修改

--- program/xinrongys/dataClass/CustomerForm.cs
+++ program/xinrongys/dataClass/CustomerForm.cs
@@ -11,11 +11,23 @@ namespace xinrongys
 {
     public partial class CustomerForm : Form
     {
+        private Connector connect = null;
+        private List<Customer> customers = null;
+       
         public CustomerForm()
         {
             InitializeComponent();
         }
-
+        public Connector Connect
+        {
+            get { return connect; }
+            set { connect = value; }
+        }
+        public List<Customer> Customers
+        {
+            get { return customers; }
+            set { customers = value; }
+        }
         /// <summary>
         /// 新增或是修改資料
         /// </summary>
@@ -24,7 +36,30 @@ namespace xinrongys
         /// <returns></returns>
         private void enterButton_Click(object sender, EventArgs e)
         {
-            //TODO
+            Customer c = new Customer();
+            c._Addr = addrTextBox.Text;
+            c._B_account = b_accountTextBox.Text;
+            c._B_name = b_nameTextBox.Text;
+            c._City = cityTextBox.Text;
+            c._Contacter = contacterTextBox.Text;
+            c._Currency = currencyBox.Text;
+            c._Day = dayTextBox.Text;
+            c._District = districtTextBox.Text;
+            c._Ext1 = ext1TextBox.Text;
+            c._Ext2 = ext2TextBox.Text;
+            c._Ext3 = ext3TextBox.Text;
+            c._Fax = faxTextBox.Text;
+            c._Id = idTextBox.Text;
+            c._Mail = mailTextBox.Text;
+            c._Mobile = mobileTextBox.Text;
+            c._Name = nameTextBox.Text;
+            c._Phone1 = phone1TextBox.Text;
+            c._Phone2 = phone2TextBox.Text;
+            c._Province = provinceTextBox.Text;
+            c._Shortname = shortnameTextBox.Text;
+            c._Town = townTextBox.Text;
+            c._Village = villageTextBox.Text;
+            c._Zone = zoneTextBox.Text;
         }
 
         /// <summary>
