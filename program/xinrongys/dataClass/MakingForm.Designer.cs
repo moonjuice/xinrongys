db8352ef157f620e514178096e436e039c376b58
moon80362@gmail.com <moon80362@gmail.com@0febd70d-a258-cbe2-6812-9670ac12e6ed>
2011-04-05 01:55:16 +0000

    修改材料視窗長度

--- program/xinrongys/dataClass/MakingForm.Designer.cs
+++ program/xinrongys/dataClass/MakingForm.Designer.cs
@@ -67,11 +67,11 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel2.Controls.Add(this.navBarPanel, 1, 0);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 121);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 122);
             this.tableLayoutPanel2.Name = "tableLayoutPanel2";
             this.tableLayoutPanel2.RowCount = 1;
             this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(558, 30);
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(574, 30);
             this.tableLayoutPanel2.TabIndex = 3;
             // 
             // navBarPanel
@@ -84,9 +84,9 @@
             this.navBarPanel.Controls.Add(this.nextButton);
             this.navBarPanel.Controls.Add(this.lastButton);
             this.navBarPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.navBarPanel.Location = new System.Drawing.Point(170, 3);
+            this.navBarPanel.Location = new System.Drawing.Point(175, 3);
             this.navBarPanel.Name = "navBarPanel";
-            this.navBarPanel.Size = new System.Drawing.Size(217, 24);
+            this.navBarPanel.Size = new System.Drawing.Size(223, 24);
             this.navBarPanel.TabIndex = 0;
             // 
             // firstButton
@@ -187,16 +187,16 @@
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(558, 121);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(574, 122);
             this.tableLayoutPanel1.TabIndex = 4;
             // 
             // priceTextBox
             // 
             this.priceTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.priceTextBox.Font = new System.Drawing.Font("新細明體", 12F);
-            this.priceTextBox.Location = new System.Drawing.Point(383, 63);
+            this.priceTextBox.Location = new System.Drawing.Point(394, 63);
             this.priceTextBox.Name = "priceTextBox";
-            this.priceTextBox.Size = new System.Drawing.Size(172, 27);
+            this.priceTextBox.Size = new System.Drawing.Size(177, 27);
             this.priceTextBox.TabIndex = 11;
             // 
             // flowLayoutPanel2
@@ -204,15 +204,15 @@
             this.flowLayoutPanel2.Controls.Add(this.cancelButton);
             this.flowLayoutPanel2.Controls.Add(this.enterButton);
             this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(383, 93);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(394, 93);
             this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(172, 25);
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(171, 25);
             this.flowLayoutPanel2.TabIndex = 1;
             // 
             // cancelButton
             // 
             this.cancelButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.cancelButton.Location = new System.Drawing.Point(94, 3);
+            this.cancelButton.Location = new System.Drawing.Point(93, 3);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
             this.cancelButton.TabIndex = 1;
@@ -223,7 +223,7 @@
             // enterButton
             // 
             this.enterButton.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.enterButton.Location = new System.Drawing.Point(13, 3);
+            this.enterButton.Location = new System.Drawing.Point(12, 3);
             this.enterButton.Name = "enterButton";
             this.enterButton.Size = new System.Drawing.Size(75, 23);
             this.enterButton.TabIndex = 0;
@@ -241,18 +241,18 @@
             "米",
             "卷",
             "公斤"});
-            this.unitComboBox.Location = new System.Drawing.Point(120, 63);
+            this.unitComboBox.Location = new System.Drawing.Point(123, 63);
             this.unitComboBox.Name = "unitComboBox";
-            this.unitComboBox.Size = new System.Drawing.Size(140, 24);
+            this.unitComboBox.Size = new System.Drawing.Size(145, 24);
             this.unitComboBox.TabIndex = 10;
             // 
             // s_idTextBox
             // 
             this.s_idTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.s_idTextBox.Font = new System.Drawing.Font("新細明體", 12F);
-            this.s_idTextBox.Location = new System.Drawing.Point(383, 33);
+            this.s_idTextBox.Location = new System.Drawing.Point(394, 33);
             this.s_idTextBox.Name = "s_idTextBox";
-            this.s_idTextBox.Size = new System.Drawing.Size(172, 27);
+            this.s_idTextBox.Size = new System.Drawing.Size(177, 27);
             this.s_idTextBox.TabIndex = 9;
             // 
             // label2
@@ -260,9 +260,9 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 12F);
-            this.label2.Location = new System.Drawing.Point(266, 7);
+            this.label2.Location = new System.Drawing.Point(274, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.Size = new System.Drawing.Size(114, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "材  料  名 稱：";
             // 
@@ -273,7 +273,7 @@
             this.label1.Font = new System.Drawing.Font("新細明體", 12F);
             this.label1.Location = new System.Drawing.Point(3, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.Size = new System.Drawing.Size(114, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "材 料 編 號 ：";
             // 
@@ -281,9 +281,9 @@
             // 
             this.idTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.idTextBox.Font = new System.Drawing.Font("新細明體", 12F);
-            this.idTextBox.Location = new System.Drawing.Point(120, 3);
+            this.idTextBox.Location = new System.Drawing.Point(123, 3);
             this.idTextBox.Name = "idTextBox";
-            this.idTextBox.Size = new System.Drawing.Size(140, 27);
+            this.idTextBox.Size = new System.Drawing.Size(145, 27);
             this.idTextBox.TabIndex = 1;
             // 
             // label3
@@ -293,7 +293,7 @@
             this.label3.Font = new System.Drawing.Font("新細明體", 12F);
             this.label3.Location = new System.Drawing.Point(3, 37);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(111, 16);
+            this.label3.Size = new System.Drawing.Size(114, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "材 料 種 類 ：";
             // 
@@ -302,9 +302,9 @@
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("新細明體", 12F);
-            this.label4.Location = new System.Drawing.Point(266, 37);
+            this.label4.Location = new System.Drawing.Point(274, 37);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 16);
+            this.label4.Size = new System.Drawing.Size(114, 16);
             this.label4.TabIndex = 4;
             this.label4.Text = "供應商編號 ：";
             // 
@@ -315,7 +315,7 @@
             this.label5.Font = new System.Drawing.Font("新細明體", 12F);
             this.label5.Location = new System.Drawing.Point(3, 67);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(111, 16);
+            this.label5.Size = new System.Drawing.Size(114, 16);
             this.label5.TabIndex = 5;
             this.label5.Text = "單  　位  　：";
             // 
@@ -324,9 +324,9 @@
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("新細明體", 12F);
-            this.label6.Location = new System.Drawing.Point(266, 67);
+            this.label6.Location = new System.Drawing.Point(274, 67);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(111, 16);
+            this.label6.Size = new System.Drawing.Size(114, 16);
             this.label6.TabIndex = 6;
             this.label6.Text = "單       價      ：";
             // 
@@ -334,9 +334,9 @@
             // 
             this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.nameTextBox.Font = new System.Drawing.Font("新細明體", 12F);
-            this.nameTextBox.Location = new System.Drawing.Point(383, 3);
+            this.nameTextBox.Location = new System.Drawing.Point(394, 3);
             this.nameTextBox.Name = "nameTextBox";
-            this.nameTextBox.Size = new System.Drawing.Size(172, 27);
+            this.nameTextBox.Size = new System.Drawing.Size(177, 27);
             this.nameTextBox.TabIndex = 7;
             // 
             // typeComboBox
@@ -350,18 +350,19 @@
             "膜類",
             "雙面膠類",
             "其他類"});
-            this.typeComboBox.Location = new System.Drawing.Point(120, 33);
+            this.typeComboBox.Location = new System.Drawing.Point(123, 33);
             this.typeComboBox.Name = "typeComboBox";
-            this.typeComboBox.Size = new System.Drawing.Size(140, 24);
+            this.typeComboBox.Size = new System.Drawing.Size(145, 24);
             this.typeComboBox.TabIndex = 8;
             // 
             // MakingForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 151);
+            this.ClientSize = new System.Drawing.Size(574, 152);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.tableLayoutPanel2);
+            this.Font = new System.Drawing.Font("新細明體", 9F);
             this.Name = "MakingForm";
             this.Text = "材料基本資料";
             this.tableLayoutPanel2.ResumeLayout(false);
